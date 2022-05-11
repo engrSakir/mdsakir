a2fe6959c48f61019e91bfe09b55f7cccf17a759
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-05-12 00:45:40 +0600

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,7 @@ class Navigation extends Component {
             <BrowserRouter>
                 <Navbar bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/">mdsakir.com</Navbar.Brand>
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
