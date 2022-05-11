823896adf2af792a01eb827389e67bb2f7f3867a
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-05-12 01:20:32 +0600

    marquee

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import Breadcrumb from "../components/Breadcrumb";
+import {Card} from "react-bootstrap";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Breadcrumb title={'Home'}/>
+                <Card body>
+                    <marquee scrolldirection="left">
+                        Md. Sakir Ahmed | Web application developer | Skills: PHP, Laravel, Livewire, React JS, Vue JS, jQuery, JS, CSS HTML | Contact +880 1304-734623 m.sakirahmed@gmail.com
+                    </marquee>
+                </Card>
             </div>
         );
     }
